cd78ff8dd126a6bdb22c647d0ec0726d6af11505
阿維 <lairool2001@yahoo.com.tw>
2025-10-27 21:04:55 +0800

    搜尋器

---
+++ SearcherXX1/Class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+public class RichTextBoxHelper
+{
+    [DllImport("user32.dll")]
+    private static extern int SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+    // 定義 SCROLLINFO 結構
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SCROLLINFO
+    {
+        public uint cbSize;
+        public uint fMask;
+        public int nMin;
+        public int nMax;
+        public uint nPage;
+        public int nPos;       // 滾動偏移
+        public int nTrackPos;  // 拖動時偏移
+    }    // 常數
+    private const int WM_VSCROLL = 0x115;
+    private const int SB_THUMBPOSITION = 4;
+    private const int SB_THUMBTRACK = 5;
+
+    // API 函數
+    [DllImport("user32.dll")]
+    static extern bool GetScrollInfo(IntPtr hwnd, int bar, ref SCROLLINFO scrollInfo);
+
+    // 滾動到特定偏移行數（例如：第n行）
+    public static void ScrollToLine(RichTextBox rtb, int lineIndex)
+    {
+        if (lineIndex < 0 || lineIndex >= rtb.Lines.Length)
+            return;
+        // 每行高度假設為固定值
+        // 更為精確的方法是使用 GetPositionFromCharIndex
+        int charIndex = rtb.GetFirstCharIndexFromLine(lineIndex);
+        rtb.SelectionStart = charIndex;
+        rtb.ScrollToCaret(); // 使游標所在位置置底
+    }
+    /// <summary>
+    /// 滾動到指定進度（0.0 到 1.0）
+    /// </summary>
+    public static void ScrollToProgress(RichTextBox rtb, double progress) // progress: 0.0 ~ 1.0
+    {
+        if (progress < 0.0 || progress > 1.0)
+            throw new ArgumentOutOfRangeException(nameof(progress), "Must be between 0.0 and 1.0");
+        // 取得滾動資訊
+        SCROLLINFO si = new SCROLLINFO();
+        si.cbSize = (uint)Marshal.SizeOf(typeof(SCROLLINFO));
+        si.fMask = 0x10; // SIF_POS | SIF_RANGE | SIF_PAGE
+        if (GetScrollInfo(rtb.Handle, 1 /* SB_VERT */, ref si))
+        {
+            int maxScroll = si.nMax - (int)si.nPage + 1; // 滾動範圍
+            int targetPos = (int)(progress * maxScroll);
+            // 設定滾動位置
+            SendMessage(rtb.Handle, WM_VSCROLL, new IntPtr(SB_THUMBPOSITION), new IntPtr(targetPos));
+        }
+    }
+    // 讀取垂直滾動位置
+    public static int GetCurrentScrollPosition(RichTextBox rtb)
+    {
+        SCROLLINFO si = new SCROLLINFO();
+        si.cbSize = (uint)Marshal.SizeOf(typeof(SCROLLINFO));
+        si.fMask = 0x10; // SIF_POS | SIF_RANGE | SIF_PAGE
+        if (GetScrollInfo(rtb.Handle, 1, ref si))
+            return si.nPos;
+        return 0;
+    }
+    // 設定滾動位置
+    public static void SetScrollPosition(RichTextBox rtb, int position)
+    {
+        SendMessage(rtb.Handle, WM_VSCROLL, new IntPtr(SB_THUMBPOSITION), new IntPtr(position));
+    }
+}
---
+++ SearcherXX1/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace SearcherXX1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(575, 91);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox2.DetectUrls = false;
+            this.richTextBox2.Location = new System.Drawing.Point(12, 109);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox2.Size = new System.Drawing.Size(821, 834);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.richTextBox2_LinkClicked);
+            this.richTextBox2.VScroll += new System.EventHandler(this.richTextBox2_VScroll);
+            this.richTextBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.richTextBox2_MouseDown);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("新細明體", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(593, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(240, 91);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "搜尋(NTFS除外)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 955);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
