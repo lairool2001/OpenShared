352c28b3bb7019a941a88c1239334b7507b20e6b
阿維 <lairool2001@yahoo.com.tw>
2025-11-26 17:37:02 +0800

    小執行腳本

---
+++ BigShared/SmallHahaScript/NativeShellExecutor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices; // 必須引用：用於 DllImport
+
+public class NativeShellExecutor
+{
+    // 1. 宣告 Windows API (ShellExecute)
+    [DllImport("shell32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    public static extern IntPtr ShellExecute(
+        IntPtr hwnd,        // 視窗控制代碼 (通常設為 IntPtr.Zero)
+        string lpOperation, // 操作動作："open", "print", "runas" (系統管理員)
+        string lpFile,      // 要執行的檔案路徑
+        string lpParameters,// 參數 (如果有的話)
+        string lpDirectory, // 工作目錄 (通常設為 null，使用預設)
+        int nShowCmd        // 視窗顯示狀態 (1 = 正常顯示, 0 = 隱藏)
+    );
+
+    // 定義視窗顯示常數
+    private const int SW_SHOWNORMAL = 1;
+
+    // 2. 包裝成一個簡單的方法
+    public static void Execute(string path)
+    {
+        string args = $"\"{path}\"";
+
+        // 使用 explorer.exe 來開啟檔案
+        Process.Start("explorer.exe", args);
+    }
+}
+
+// === 使用範例 ===
+// public static void Main()
+// {
+//     string batPath = @"D:\YourScript.bat";
+//     NativeShellExecutor.Execute(batPath);
+// }
---
+++ BigShared/SmallHahaScript/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualBasic;
+using System.Diagnostics;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//get arg
+if (args.Length > 0)
+{
+    string path = args[0];
+    Console.WriteLine("Argument: " + path);
+    string[] linez = File.ReadAllText(path).Replace("\r", "").Split("\n");
+    for (int i = 0; i < linez.Length; i++)
+    {
+        string line = linez[i];
+        string[] lineArgz = line.Split(' ');
+        if (lineArgz.Length < 2) continue;
+        string command = lineArgz[0];
+        string data = lineArgz[1];
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine(command);
+        try
+        {
+            switch (command)
+            {
+                case "執行":
+                    string fileName = data.Replace("\"", "");
+                    string exetype = Path.GetExtension(fileName).ToLower();
+                    string folder = Path.GetDirectoryName(fileName);
+                    Process.Start(new ProcessStartInfo()
+                    {
+                        FileName = fileName,
+                        UseShellExecute = true,
+                        WorkingDirectory = folder
+                    });
+                    break;
+                case "等待秒":
+                    float waitTime = float.Parse(data);
+                    Thread.Sleep((int)(waitTime * 1000));
+                    break;
+                default:
+                    break;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Error executing command: " + ex.Message);
+        }
+    }
+    Console.WriteLine("完成");
+    Console.ReadLine();
+}
