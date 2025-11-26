095e9528d4573fee2a669509ddabe01c73528c03
阿維 <lairool2001@yahoo.com.tw>
2025-11-26 22:56:52 +0800

    shhs

--- BigShared/SmallHahaScript/Program.cs
+++ BigShared/SmallHahaScript/Program.cs
@@ -15,60 +15,63 @@ if (args.Length > 0)
         string command = lineArgz[0];
         string data = lineArgz[1];
         Console.ForegroundColor = ConsoleColor.White;
-        Console.WriteLine(command);
-        GoCommand(command, data);
-    }
-    Console.WriteLine("完成");
-    Console.ReadLine();
-}
-
-static void GoCommand(string command, string data)
-{
-    try
-    {
-        switch (command)
+        Console.WriteLine(command); try
         {
-            case "執行":
-                string fileName = data.Replace("\"", "");
-                string exetype = Path.GetExtension(fileName).ToLower();
-                string folder = Path.GetDirectoryName(fileName);
-                Process.Start(new ProcessStartInfo()
-                {
-                    FileName = fileName,
-                    UseShellExecute = true,
-                    WorkingDirectory = folder
-                });
-                break;
-            case "等待秒":
-                float waitTime = float.Parse(data);
-                Thread.Sleep((int)(waitTime * 1000));
-                break;
-            case "命令台":
-                while (true)
-                {
-                    Console.WriteLine("命令:");
-                    string arg1 = Console.ReadLine();
-                    if (arg1 == "結束") break;
-                    Console.WriteLine("資料:");
-                    string arg2 = Console.ReadLine();
-                    try
-                    {
-                        GoCommand(arg1, arg2);
-                    }
-                    catch
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("命令執行失敗");
-                    }
-                }
-                break;
-            default:
-                break;
+            GoCommand(command, data);
         }
-    }
     catch (Exception ex)
     {
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine("Error executing command: " + ex.Message);
+    }
+}
+Console.WriteLine("完成");
+Console.ReadLine();
+}
+
+static void GoCommand(string command, string data)
+{
+
+    switch (command)
+    {
+        case "執行":
+            string fileName = data.Replace("\"", "");
+            string exetype = Path.GetExtension(fileName).ToLower();
+            string folder = Path.GetDirectoryName(fileName);
+            Process.Start(new ProcessStartInfo()
+            {
+                FileName = fileName,
+                UseShellExecute = true,
+                WorkingDirectory = folder
+            });
+            break;
+        case "等待秒":
+            float waitTime = float.Parse(data);
+            Thread.Sleep((int)(waitTime * 1000));
+            break;
+        case "命令台":
+            while (true)
+            {
+                Console.WriteLine("命令:");
+                string arg1 = Console.ReadLine();
+                if (arg1 == "結束") break;
+                Console.WriteLine("資料:");
+                string arg2 = Console.ReadLine();
+                try
+                {
+                    GoCommand(arg1, arg2);
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("命令執行失敗");
+                }
+            }
+            break;
+        case "清空":
+            Console.Clear();
+            break;
+        default:
+            break;
     }
 }
